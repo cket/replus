81937906190da567b6e5436efb50f65555dfebd2
CJ Ketchum <CJ@Narsil.local>
2016-11-02 20:48:01 -0700

    Checkbox can now be properly unchecked

--- myscript.js
+++ myscript.js
@@ -11,6 +11,7 @@ console.log('Script execution');
 var comments = document.getElementsByClassName("review-comment js-comment js-task-list-container commit-comment member-comment  previewable-edit  js-reorderable-task-lists reorderable-task-lists");
 var needListeners = document.getElementsByClassName("file js-comment-container has-inline-notes");
 if(typeof comments == 'undefined'){
+	// javascipt has no exit? 
 	throw new Error();
 }
 var main_review = needListeners[0];
@@ -22,12 +23,25 @@ for (i = 0; i < comments.length; i++){
 	checkbox.type = "checkbox";
 	checkbox.name = "resolved";
 	checkbox.value = "value";
-	checkbox.id = 'cjk'.concat(String(i)); 
+	checkbox.id = 'cjk'.concat(String(i));
+	// check storage to see if this was clicked before page was reloaded 
 	comments[i].appendChild(checkbox);
 };
 function markResolved(event){
-	console.log(this);
-	console.log(event);
-	var target = document.getElementById(event.target.id).parentNode;
-	target.style.color = "blue";
+	var checkbox = document.getElementById(event.target.id);
+	var target = checkbox.parentNode;
+	// careful not to clash with built in 'checked'
+	if(typeof checkbox.is_checked == 'undefined'){
+		checkbox.is_checked = true;
+	}
+	if(checkbox.is_checked){
+		console.log('Now Blue');
+		target.style.color = "blue";
+		checkbox.is_checked = false;
+	} else {
+		console.log('Now Black');
+		target.style.color = "black";
+		checkbox.is_checked = true;
+	}
+	
 }
