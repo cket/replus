a3bbd0baff1ebfbd7474b515e2f75cf282975577
CJ Ketchum <CJ@Narsil.local>
2016-11-02 17:42:41 -0700

    Mark review comments when clicked

--- myscript.js
+++ myscript.js
@@ -1,65 +1,33 @@
-console.log('execution, at least');
-var elems = document.getElementsByClassName("timeline-comment-actions");
+// heirarchy of the page:
+// "timeline-comment js-comment"
+// "discussion-item-body"
+// "file js-comment-container has-inline-notes"
+// "js-inline-comments-container review-comments"
+// "js-comments-holder"
+// "edit-comment-hide" //main class we want to mess with
+// "review-comment-contents"
 
-for (i = 0; i < elems.length; i++){
+console.log('Script execution');
+var comments = document.getElementsByClassName("review-comment js-comment js-task-list-container commit-comment member-comment  previewable-edit  js-reorderable-task-lists reorderable-task-lists");
+var needListeners = document.getElementsByClassName("file js-comment-container has-inline-notes");
+if(typeof comments == 'undefined'){
+	throw new Error();
+}
+var main_review = needListeners[0];
+
+main_review.addEventListener('click', markResolved, true);
+
+for (i = 0; i < comments.length; i++){
 	var checkbox = document.createElement('input');
 	checkbox.type = "checkbox";
 	checkbox.name = "resolved";
 	checkbox.value = "value";
 	checkbox.id = 'cjk'.concat(String(i)); 
-	elems[i].appendChild(checkbox);
+	comments[i].appendChild(checkbox);
 };
-console.log('content loaded ');
-for (i = 0; i < elems.length; i++){
-	var j = i;
-	console.log('adding listers at index:');
-	console.log(i);
-	var link = document.getElementById('cjk'.concat(String(i)));
-    link.addEventListener('click', function() {
-    	markResolved(0);
-	});
+function markResolved(event){
+	console.log(this);
+	console.log(event);
+	var target = document.getElementById(event.target.id).parentNode;
+	target.style.color = "blue";
 }
-function markResolved(index){
-	console.log('At index:');
-	console.log(index);
-	var commentBody = document.getElementsByClassName("comment-body markdown-body markdown-format js-comment-body");
-	if(typeof commentBody[index] != 'undefined'){
-		console.log('marking as resolved');
-		console.log(commentBody[index].innerHTML);
-		children = document.querySelectorAll(".comment-body markdown-body markdown-format js-comment-body");
-		// commentBody[index].innerHTML = '~'.concat(commentBody[index].innerHTML, '~');
-		//currently works techinically but the markdown isn't evaluated.. we have to map every char into it's 
-		// crossed out equivalent. Alternatively color it.
-	}
-}
-// function toggleState() {
-// 	console.log('saving settings');
-// 	// Get a value saved in a form.
-// 	var theValue = chrome.storage.get('value', function(objects){
-// 		// Save it using the Chrome extension storage API.
-// 		var value = (typeof objects.value != 'undefined' ? objects.value : true)
-// 		chrome.storage.sync.set({'value': value}, function() {
-// 		// Notify that we saved.
-// 		message('Value saved');
-// 		});
-// 	});
-// };
-
-//add new toggle-able button 'resolved'
-
-//document.body.innerHTML = document.body.innerHTML.replace(new RegExp("better", "g"), "nobody");
-
-
-var replaceTextInNode = function(parentNode){
-	for(var i = parentNode.childNodes.length-1; i >= 0; i--){
-		var node = parentNode.childNodes[i];
-		if(node.nodeType == Element.TEXT_NODE){
-			node.textContent = 'HI';
-		} else if(node.nodeType == Element.ELEMENT_NODE){
-			replaceTextInNode(node);
-		}
-	}
-};
-
-//replaceTextInNode(document.body);
-
--- onClickedScript.js
+++
@@ -1,3 +0,0 @@
-console.log('marking as resolved');
-var commentBody = document.getElementsByClassName("comment-body markdown-body markdown-format js-comment-body");
-commentBody[index].p = '~'.concat(commentBody[index].p, '~');
