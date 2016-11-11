1a0dd2322417754a0974f2ea5694078848f13457
CJ Ketchum <CJ@Narsil.local>
2016-11-11 14:06:30 -0800

    State is now persistent

--- myscript.js
+++ myscript.js
@@ -24,28 +24,59 @@ for (i = 0; i < comments.length; i++){
 	checkbox.type = "checkbox";
 	checkbox.name = "resolved";
 	checkbox.value = "value";
-	checkbox.id = 'cjk'.concat(String(i));
-	// check storage to see if this was clicked before page was reloaded 
+	var id_value =  'cjk'.concat(String(i));
+	checkbox.id = id_value;
+	var is_checked;
+	chrome.storage.local.get(id_value, function(result){ 
+		console.log(JSON.stringify(result)); // figure out how to delete from storage..	
+		is_checked = result[this]; 
+		console.log(is_checked);
+		if(is_checked==true){
+			var checkbox = document.getElementById(this);
+			if(typeof checkbox == 'undefined' || checkbox.type != 'checkbox'){
+				return;
+			}
+			checkbox.checked=true;
+			_check(checkbox, this);
+		}
+	}.bind(id_value));
 	comments[i].appendChild(checkbox);
 };
+
 function markResolved(event){
 	var checkbox = document.getElementById(event.target.id);
+	_check(checkbox, event.target.id);
+}
+
+function _check(checkbox, id){
 	if(typeof checkbox == 'undefined' || checkbox.type != "checkbox"){
 		return;
 	}
 	var target = checkbox.parentNode;
-	// careful not to clash with built in 'checked'
+	// not to clash with built in 'checked'
 	if(typeof checkbox.is_checked == 'undefined'){
-		checkbox.is_checked = true;
-	}
-	if(checkbox.is_checked){
-		console.log('Now Blue');
-		target.style.color = "blue";
 		checkbox.is_checked = false;
-	} else {
-		console.log('Now Black');
-		target.style.color = "black";
-		checkbox.is_checked = true;
 	}
-	
+	// FIXME: need better key. hash of content + comment id?
+	var key = String(id);
+	console.log(key);
+	if(checkbox.is_checked != true){
+		console.log('True');
+		target.style.color = "blue";
+		checkbox.is_checked = true;
+		var obj = {};
+		obj[key] = true;
+		chrome.storage.local.set(obj, function(){
+			console.log('saved');
+		});
+	} else {
+		console.log('False');
+		target.style.color = "black";
+		checkbox.is_checked = false;
+		var obj = {};
+		obj[key] = false;
+		chrome.storage.local.set(obj, function(){
+			console.log('saved');
+		});
+	}
 }
