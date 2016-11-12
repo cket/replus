02520b5857da8985738d3b56d55993572f107fa0
CJ Ketchum <CJ@Narsil.local>
2016-11-12 13:48:50 -0800

    Add Apache 2.0 license

--- myscript.js
+++ myscript.js
@@ -1,11 +1,16 @@
-// heirarchy of the page:
-// "timeline-comment js-comment"
-// "discussion-item-body"
-// "file js-comment-container has-inline-notes"
-// "js-inline-comments-container review-comments"
-// "js-comments-holder"
-// "edit-comment-hide" //main class we want to mess with
-// "review-comment-contents"
+// Copyright 2016 Christopher Ketchum
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 console.log('Script execution');
 var comments = document.getElementsByClassName("review-comment js-comment js-task-list-container commit-comment  previewable-edit  ");
